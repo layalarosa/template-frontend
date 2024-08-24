268052df8055d6abfe812536eb6a030be9118c7f
Luis Alberto <layalarosa@gmail.com>
2024-08-24 19:29:28 -0400

    Add: config and dummy code

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+
+}
