e7e4542be74f741eac23e09397093c17fb5dcd70
Luis Alberto <layalarosa@gmail.com>
2024-08-27 22:23:41 -0400

    Add: Landing page and About page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { CurrencyPipe, DatePipe, NgFor, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./footer/footer.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgFor, NavbarComponent, RouterOutlet],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgFor, NavbarComponent, RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
